f962a9212b23c7684e7b146ef3c09b787a174a24
Serhii <malgothsm@gmail.com>
2021-02-28 22:53:10 +0200

    finished animations

--- Responsive Navigation Bar/nav.js
+++ Responsive Navigation Bar/nav.js
@@ -1,8 +1,17 @@
 const navSlide = () =>{
   const hamburger = document.querySelector('.hamburger');
   const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
   hamburger.addEventListener('click', () =>{
     nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      (link.style.animation) ? link.style.animation = '' : link.style.animation = `navLinkFading 0.6s ease forwards ${index / 7 + 0.5}s`;
+      
+    });
   });
+
+ 
 };
 navSlide();

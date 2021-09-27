0d90ee8ad955557489c337c684c670e3841fee86
Serhii <malgothsm@gmail.com>
2021-09-27 19:07:20 +0300

    Fixed bug

--- Advanced CSS Dropdown Menu/app.js
+++ Advanced CSS Dropdown Menu/app.js
@@ -1,9 +1,9 @@
 document.addEventListener('click', e => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]")
-  if (!isDropdownButton && e.target.closest("[data-dropdown]") !==null) return
+  const isDropdownButton = e.target.matches('[data-dropdown-button]')
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
   let currentDropdown
   if (isDropdownButton) {
-    currentDropdown =  e.closest("[data-dropdown]")
+    currentDropdown =  e.target.closest("[data-dropdown]")
     currentDropdown.classList.toggle('active')
   }
 
